87df680904a21872d7b23ec1a04a12b2536f8c6b
aforouzan631-coder <aforouzan631@gmail.com>
2026-06-16 19:43:38 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -63,9 +63,10 @@ def ai(user_id, text):
     prompt = "\n".join(history + [text])
 
     response = client.models.generate_content(
-    model="gemini-2.5-flash"
+    model="gemini-2.5-flash",
     contents=text
     )
+
     
 
     memory[user_id].append(text)
