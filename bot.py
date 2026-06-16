975f823fa7080dad9be2f8981e519956175a74b5
aforouzan631-coder <aforouzan631@gmail.com>
2026-06-16 17:53:43 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel("gemini-1.5-pro")
+model="gemini-1.0-pro"
 
 
 
