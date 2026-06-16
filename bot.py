7f094e81ddb20d5e42a382c4c37b3d29e38d6ccb
aforouzan631-coder <aforouzan631@gmail.com>
2026-06-16 16:49:17 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,17 +3,22 @@ from telegram import Update
 from telegram.ext import Application, MessageHandler, filters, ContextTypes
 import google.generativeai as genai
 
-# API KEY
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
 model = genai.GenerativeModel("gemini-1.5-flash")
 
-# chat function
 async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    text = update.message.text
-    response = model.generate_content(text)
-    await update.message.reply_text(response.text)
+    try:
+        text = update.message.text
 
-# bot
+        response = model.generate_content(text)
+
+        await update.message.reply_text(response.text)
+
+    except Exception as e:
+        await update.message.reply_text(f"Error: {str(e)}")
+
+
 app = Application.builder().token(os.getenv("BOT_TOKEN")).build()
 
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, chat))
